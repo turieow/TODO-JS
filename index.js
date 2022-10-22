5fdc681d3a5da35238cf40e2b815f9db98bcdaa3
aaaa <mai_19n1102996@nnn.ed.jp>
2022-10-22 20:16:25 +0900

    TODOリスト完成

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ function add(todo) {
         const li =document.createElement("li");
         li.innerText=todoText;
         li.classList.add("list-group-item");
+
+        if(todo && todo.completed) {
+          li.classList.add("text-decoration-line-through");
+        }
+
+        li.addEventListener("contextmenu", function (event){
+          event.preventDefault();
+          li.remove();
+          saveData();
+        });
+
+        li.addEventListener("click", function () {
+          li.classList.toggle("text-decoration-line-through");
+          saveData();
+        });
         ul.appendChild(li);
         input.value="";
         saveData();
